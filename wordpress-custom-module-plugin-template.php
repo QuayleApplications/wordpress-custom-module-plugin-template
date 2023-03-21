329a1d988f0dae72f51097fab34e710b53099236
Jim Floss <jfloss@insivia.com>
2023-03-21 06:41:09 -0400

    Added support for BootstrapJS 5.2.3, updated support for BootstrapCSS from 5.0.2 to 5.2.3, added support for Font Awesome 6.3.0

--- wordpress-custom-module-plugin-template.php
+++ wordpress-custom-module-plugin-template.php
@@ -22,9 +22,12 @@ function qcmptLoadScripts() {
     $js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'assets/js/app.js' ));
     $css_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/styles.css' ));
 
-    wp_enqueue_script( 'qcmpt-js', plugins_url( 'assets/js/app.js', __FILE__ ), array('jquery'), $js_ver, true );
-    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), '5.0.2', 'all' );
-    wp_enqueue_style( 'qcmpt-style', plugins_url( 'assets/css/styles.css', __FILE__ ), array('bootstrap-css'), $css_ver, 'all' );
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), '5.2.3', true );
+    wp_enqueue_script( 'qcmpt-js', plugins_url( 'assets/js/app.js', __FILE__ ), array('jquery', 'bootstrap-js'), $js_ver, true );
+   
+    wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', array(), '5.2.3', 'all' );
+    wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css', array(), '6.3.0', 'all' );
+    wp_enqueue_style( 'qcmpt-style', plugins_url( 'assets/css/styles.css', __FILE__ ), array('bootstrap-css', 'font-awesome'), $css_ver, 'all' );
 
     wp_localize_script( 'qcmpt-js', 'qcmpt_ajax', array(
         'ajaxurl' => admin_url( 'admin-ajax.php' ),
